1dfd4f8c4ea7c92549c78504a92ea26e5e2daa9d
MoustafaHp <mostafahp44@gmail.com>
2023-02-20 13:04:09 +0200

    Update Box1.js

--- src/compomnents/Box1/Box1.js
+++ src/compomnents/Box1/Box1.js
@@ -17,7 +17,7 @@ const[pass,setpass]=useState('')
   async function handlelogin(e){
     e.preventDefault()
 
-     await axios.post('/postuser',{
+     await axios.get('/home',{
         email:user,
         password:pass
       }).then(()=>{console.log('Done!')}).catch((err)=>{console.log('err');})
@@ -32,7 +32,7 @@ const[pass,setpass]=useState('')
         <i className='logo' role='img'></i>
     </div>
    
-<form method="post" action="/" >
+<form method="get" action="/" >
 <input  type='text' placeholder='Phone number, username,or email' value={user}  onChange={(e)=>{setuser(e.target.value)}}/>
 <input  type='password' placeholder='Password' value={pass} onChange={(e)=>{setpass(e.target.value)}} />
 <button className="action" type="submit" onClick={handlelogin}>
